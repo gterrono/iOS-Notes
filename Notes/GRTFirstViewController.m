ec2cf30faaebbec411d51334be44e06e62914cb0
Greg Terrono <gterrono@seas.upenn.edu>
2012-10-14 00:41:25 -0400

    Finished the map tab

--- Notes/GRTFirstViewController.m
+++ Notes/GRTFirstViewController.m
@@ -10,31 +10,46 @@
 
 @interface GRTFirstViewController ()
 @property (weak, nonatomic) IBOutlet MKMapView *map;
-
+- (void)setDefaultCenter;
 @end
 
 @implementation GRTFirstViewController
 
 - (void)viewDidLoad
 {
+    [self setDefaultCenter];
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)viewWillAppear:(BOOL)animated {
+    [self setDefaultCenter];
+    
+    [_map removeAnnotations:_map.annotations];
     NSMutableArray *notes = ((GRTAppDelegate *)[[UIApplication sharedApplication] delegate]).notes;
     for (int i=0; i<notes.count; i++) {
         GRTNote *note = (GRTNote *)notes[i];
-        [_map setCenterCoordinate:note.location.coordinate];
-        MKPointAnnotation* annot = [[MKPointAnnotation alloc] init];
-        [annot setCoordinate: note.location.coordinate];
-        [annot setTitle:note.title];
-        [self.map addAnnotation:annot];
+        if (note.location != nil) {
+            [_map setCenterCoordinate:note.location.coordinate];
+            MKPointAnnotation* annot = [[MKPointAnnotation alloc] init];
+            annot.coordinate = note.location.coordinate;
+            annot.title = note.title;
+            [self.map addAnnotation:annot];
+        }
     }
-    
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (void)setDefaultCenter {
+    CLLocationCoordinate2D location;
+    location.latitude = 37.3323;
+    location.longitude = -122.0312;
+    [_map setCenterCoordinate:location];
 }
 
 @end
